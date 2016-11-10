ecf27e2dad70985989c1033abf054f9927e2530a
Emory Merryman <emory.merryman@gmail.com>
2016-11-10 12:11:44 +0000

    use global config

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ apk update &&
 	apk add git &&
 	apk add docker &&
 	apk add bash &&
-	git config user.email "${GIT_EMAIL}" &&
-	git config user.name "${GIT_NAME}" &&
+	git config --global user.email "${GIT_EMAIL}" &&
+	git config --global user.name "${GIT_NAME}" &&
 	true
 
