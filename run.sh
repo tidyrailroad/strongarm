64a5866858c01aab333a6f6e18cceb3c555e3b43
root <root@myprecise.box>
2016-11-20 20:01:17 +0000

    revising

--- run.sh
+++ run.sh
@@ -8,6 +8,13 @@ dnf update --assumeyes &&
 	dnf install --assumeyes openssh &&
 	dnf install --assumeyes git &&
 	dnf install --assumeyes bash &&
+	dnf install --assumeyes bash-completion &&
+	dnf install --assumeyes docker-zsh-completion &&
+	dnf install --assumeyes docker-fish-completion &&
+	dnf install --assumeyes docker-latest-zsh-completion &&
+	dnf install --assumeyes git-tools &&
+	dnf install --assumeyes gitflow &&
+	dnf install --assumeyes meld &&
 	git config --global user.email "${GIT_EMAIL}" &&
 	git config --global user.name "${GIT_NAME}" &&
 	dnf update --assumeyes &&
