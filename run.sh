5eaa169bbd5a5d98f44d30454ebb3cf414a2487d
Emory Merryman <emory.merryman@gmail.com>
2016-12-11 13:01:46 +0000

    use alpine

--- run.sh
+++ run.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
-dnf update --assumeyes &&
-	cp /opt/docker/docker.repo /etc/yum.repos.d &&
-	chmod 0644 /etc/yum.repos.d/docker.repo &&
-	dnf install --assumeyes docker-engine &&
-	systemctl enable docker.service &&
-	dnf install --assumeyes openssh &&
-	dnf install --assumeyes git &&
-	dnf install --assumeyes bash &&
-	dnf install --assumeyes bash-completion &&
-	dnf install --assumeyes git-tools &&
-	dnf install --assumeyes gitflow &&
-	dnf install --assumeyes meld &&
-	git config --global user.email "${GIT_EMAIL}" &&
-	git config --global user.name "${GIT_NAME}" &&
-	dnf update --assumeyes &&
-	dnf clean all &&
-	true
+apk update &&
+    apk upgrade &&
+    apk add docker &&
+    rm --recursive --force /opt/docker &&
+    true
 
